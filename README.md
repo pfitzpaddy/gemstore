a71ca64d5bd2dda2cdaa18dac0316babbc4a98d9
pfitzgerald <Patrick.Fitzgerald@agoda.com>
2014-09-02 15:35:12 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Gem Store
+--------
+Experiements Backoffice
+
+### Prerequisites
+---
+- git
+- bower
+- npm
+
+### Scaffolding
+---
+To set up the project and dependencies
+
+1. Install front end dependencies (html, css). These are specified in the "*bower.json*" file and include Angular.js, jQuery and Bootstrap.
+	
+		bower install
+
+2. Install Node dependencies. These are specified in the "*package.json*" file and include *node-web-server*.
+	
+		npm install
+
+3. Start the local web server and navigate to http://localhost:8000/index.html, web server configuration specified in *Grunt.js*.
+	
+		grunt web_server
+
+### Branching
+---
+Info here on branching
+
+- feature
+- bugfix
+- hotfix
---
+++ app/js/products.js
@@ -0,0 +1,68 @@
+(function(){
+    var app = angular.module('store-directives', []);
+
+    app.directive("productDescription", function() {
+      return {
+        restrict: 'E',
+        templateUrl: "partials/product-description.html"
+      };
+    });
+
+    app.directive("productReviews", function() {
+      return {
+        restrict: 'E',
+        templateUrl: "partials/product-reviews.html",
+        controller: function() {
+          this.review = {};
+
+          this.addReview = function(product){
+            this.review.createdOn = Date.now();
+            product.reviews.push(this.review);
+            this.review = {};
+          }
+        },
+        controllerAs: "reviewCtrl"
+      };
+    });
+
+    app.directive("productSpecs", function() {
+      return {
+        restrict:"A",
+        templateUrl: "partials/product-specs.html",
+        controllerAs: "product"
+      };
+    });
+
+    app.directive("productTabs", function() {
+      return {
+        restrict: "E",
+        templateUrl: "partials/product-tabs.html",
+        controller: function() {
+          this.tab = 1;
+
+          this.isSet = function(checkTab) {
+            return this.tab === checkTab;
+          };
+
+          this.setTab = function(activeTab) {
+            this.tab = activeTab;
+          };
+        },
+        controllerAs: "tab"
+      };
+    });
+
+    app.directive("productGallery", function() {
+      return {
+        restrict: "E",
+        templateUrl: "partials/product-gallery.html",
+        controller: function() {
+          this.current = 0;
+          this.setCurrent = function(imageNumber){
+            this.current = imageNumber || 0;
+          };
+        },
+        controllerAs: "gallery"
+      };
+    });
+  })();
